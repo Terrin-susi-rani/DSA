8af9778cf0e7bf62aaf02babf5dd190155566b69
Terrin-susi-rani <129254361+Terrin-susi-rani@users.noreply.github.com>
2025-05-29 14:17:36 +0530

    Time: 738 ms (5.01%) | Memory: 12.6 MB (91.07%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -3,20 +3,17 @@ public:
     int countPrimes(int n) {
         if (n <= 2) return 0;
         vector<bool>num(n,true);
-        vector<int>isPrime;
+        int count=0;
         num[0]=num[1]=false;
-        for(int i=2;i*i<n;++i){
-            for(int j=i*i;j<n;j+=i){
+        for(int i=2;i<n;++i){
+            if(num[i]){
+                count+=1;
+            }
+            for(int j=i*2;j<n;j+=i){
                     num[j]=false;
             }
         }
 
-        for(int i=0;i<n;i++){
-            if(num[i]){
-                isPrime.push_back(i);
-            }
-        }
-
-        return isPrime.size();
+        return count;
     }
 };
